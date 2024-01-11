43f541092ea2f9ed1b60181a89ebe1736942c9ac
enkanfenyy19 <126758030+enkanfenyy19@users.noreply.github.com>
2024-01-11 19:52:33 +0700

    app.py

--- app.py
+++ app.py
@@ -7,67 +7,101 @@ breastcancer_model = pickle.load(open('breastcancer_model.sav', 'rb'))
 #Judul web
 st.title('Aplikasi Kanker Payudara')
 
-Id = st.text_input ('Input nilai Id')
+#Membagi kolom
+col1, col2, col3 = st.columns(3)
 
-Radius_mean = st.text_input ('Input nilai Radius Mean')
+with col1 :
+    Id = st.text_input ('Input nilai Id')
 
-Texture_mean = st.text_input ('Input nilai Texture Mean')
+with col2 :
+    Radius_mean = st.text_input ('Input nilai Radius Mean')
 
-Perimeter_mean = st.text_input ('Input nilai Perimeter Mean')
+with col3 : 
+    Texture_mean = st.text_input ('Input nilai Texture Mean')
 
-Area_mean = st.text_input ('Input nilai Area Mean')
+with col1 :
+    Perimeter_mean = st.text_input ('Input nilai Perimeter Mean')
 
-Smoothness_mean = st.text_input ('Input nilai Smoothness Mean')
+with col2 :
+    Area_mean = st.text_input ('Input nilai Area Mean')
 
-Compactness_mean = st.text_input ('Input nilai Compactness Mean')
+with col3 :
+    Smoothness_mean = st.text_input ('Input nilai Smoothness Mean')
 
-Concavity_mean = st.text_input ('Input nilai Concavity Mean')
+with col1 :
+    Compactness_mean = st.text_input ('Input nilai Compactness Mean')
 
-Concave_points_mean = st.text_input ('Input nilai Concave Points Mean')
+with col2 :
+    Concavity_mean = st.text_input ('Input nilai Concavity Mean')
 
-Symmetry_mean = st.text_input ('Input nilai Symmetry Mean')
+with col3 :
+    Concave_points_mean = st.text_input ('Input nilai Concave Points Mean')
 
-Fractal_dimension_mean = st.text_input ('Input nilai Fractal Dimension Mean')
+with col1 :
+    Symmetry_mean = st.text_input ('Input nilai Symmetry Mean')
 
-Radius_se = st.text_input ('Input nilai Radius Se')
+with col2 :
+    Fractal_dimension_mean = st.text_input ('Input nilai Fractal Dimension Mean')
 
-Texture_se = st.text_input ('Input nilai Texture Se')
+with col3 :
+    Radius_se = st.text_input ('Input nilai Radius Se')
 
-Perimeter_se = st.text_input ('Input nilai Perimeter Se')
+with col1 :
+    Texture_se = st.text_input ('Input nilai Texture Se')
 
-Area_se = st.text_input ('Input nilai Area Se')
+with col2 :
+    Perimeter_se = st.text_input ('Input nilai Perimeter Se')
 
-Smoothness_se = st.text_input ('Input nilai Smoothness Se')
+with col3 :
+    Area_se = st.text_input ('Input nilai Area Se')
 
-Compactness_se = st.text_input ('Input nilai Compactness Se')
+with col1 :
+    Smoothness_se = st.text_input ('Input nilai Smoothness Se')
 
-Concavity_se = st.text_input ('Input nilai Concavity Se')
+with col2 :
+    Compactness_se = st.text_input ('Input nilai Compactness Se')
 
-Concave_points_se = st.text_input ('Input nilai Concave Points Se')
+with col3 :
+    Concavity_se = st.text_input ('Input nilai Concavity Se')
 
-Symmetry_se = st.text_input ('Input nilai Symmetry Se')
+with col1 :
+    Concave_points_se = st.text_input ('Input nilai Concave Points Se')
 
-Fractal_dimension_se = st.text_input ('Input nilai Fractal Dimension Se')
+with col2 :
+    Symmetry_se = st.text_input ('Input nilai Symmetry Se')
 
-Radius_worst = st.text_input ('Input nilai Radius Worst')
+with col3 :
+    Fractal_dimension_se = st.text_input ('Input nilai Fractal Dimension Se')
 
-Texture_worst = st.text_input ('Input nilai Texture Worst')
+with col1 :
+    Radius_worst = st.text_input ('Input nilai Radius Worst')
 
-Perimeter_worst = st.text_input ('Input nilai Perimeter Worst')
+with col2 :
+    Texture_worst = st.text_input ('Input nilai Texture Worst')
 
-Area_worst = st.text_input ('Input nilai Area Worst')
+with col3 :
+    Perimeter_worst = st.text_input ('Input nilai Perimeter Worst')
 
-Smoothness_worst = st.text_input ('Input nilai Smoothness Worst')
+with col1 :
+    Area_worst = st.text_input ('Input nilai Area Worst')
 
-Compactness_worst = st.text_input ('Input nilai Compactness Worst')
+with col2 :
+    Smoothness_worst = st.text_input ('Input nilai Smoothness Worst')
 
-Concavity_worst = st.text_input ('Input nilai Concavity Worst')
+with col3 :
+    Compactness_worst = st.text_input ('Input nilai Compactness Worst')
 
-Concave_points_worst = st.text_input ('Input nilai Concave Worst')
+with col1 :
+    Concavity_worst = st.text_input ('Input nilai Concavity Worst')
 
-Symmetry_worst = st.text_input ('Input nilai Symmetry Worst')
+with col2 :
+    Concave_points_worst = st.text_input ('Input nilai Concave Worst')
 
-Fractal_dimension_worst = st.text_input ('Input nilai Fractal Dimension Worst')
+with col3 :
+    Symmetry_worst = st.text_input ('Input nilai Symmetry Worst')
+
+with col1 :
+    Fractal_dimension_worst = st.text_input ('Input nilai Fractal Dimension Worst')
 
 #Code untuk diagnosis
 kanker_diagnosis = ''
